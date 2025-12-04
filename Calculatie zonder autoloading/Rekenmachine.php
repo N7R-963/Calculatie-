6603df43b8c0cc9052e0bacccaba38b44479ae40
N7R-963 <saied.faraa6@gmail.com>
2025-12-04 16:43:30 +0100

    Add files via upload

---
+++ Calculatie zonder autoloading/Rekenmachine.php
@@ -0,0 +1,22 @@
+<?php
+
+class Rekenmachine
+{
+    // methods
+    public function optellen($getal1, $getal2)
+    {
+        return $getal1 + $getal2;
+    }
+}
+
+
+
+
+//class Rekenmachine {
+    // properties 
+
+    
+    //methods
+//public function optellen($getal1, $getal2){
+    //return $getal1 + $getal2;
+
